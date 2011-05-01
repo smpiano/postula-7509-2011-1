15252162bb48108de6848f927b4b7cb68cd75b8a
snipperme@gmail.com <snipperme@gmail.com@aeaa8df5-d76f-69f6-2d0c-b8b2e875e448>
2011-05-01 20:20:20 +0000

    Postular pretty much working... some more test needed and trivial stuff requested

--- src/bin/beneficio_helpers.sh
+++ src/bin/beneficio_helpers.sh
@@ -1,7 +1,21 @@
-buscar_beenficio() {
-  grep "^$1," $MAESTRO_BENEFICIOS
+# Id de Beneficio, numérico
+# Código de beneficio, 5 caracteres
+# Sponsor	N caracteres, ejemplos: Banco Mundial, Naciones Unidas, Presidencia, Fontar, Fonsoft
+# Fecha Inicio Beneficio, aaaa-mm-dd
+# Fecha Fin Beneficio, aaaa-mm-dd
+# Duración máxima beneficio, numérico	
+buscar_beneficio() {
+  grep "^[^,]*,$1," $MAESTRO_BENEFICIOS
+}
+
+fecha_inicio_beneficio() {
+  echo $1 | cut -f 4 -d ,
 }
 
 fecha_baja_beneficio() {
-  echo $1 | cut -f 1 -d ,
+  echo $1 | cut -f 5 -d ,
+}
+
+duracion_maxima_beneficio() {
+  echo $1 | cut -f 6 -d ,
 }
--- src/bin/postular.sh
+++ src/bin/postular.sh
@@ -5,7 +5,7 @@ source ./beneficio_helpers.sh
 
 DIR_ARRIBOS=arribos
 
-CAMPOS_NOVEDAD=( '' CUIL 'Tipo doc' 'Nro doc' Apellido Nombre Domicilio Localidad Provincia 'Código de Beneficio' 'Fecha pedido de Alta' 'Duración pedida' )
+CAMPOS_NOVEDAD=( CUIL 'Tipo doc' 'Nro doc' Apellido Nombre Domicilio Localidad Provincia 'Código de Beneficio' 'Fecha pedido de Alta' 'Duración pedida' )
 main() {
 
   # Esto debería ser hecho sólo por postini...
@@ -29,7 +29,6 @@ main() {
     fi
 
     contador=0
-    # echo $info_agencia
     while read novedad; do
       contador=$(( $contador + 1 ))
 
@@ -37,10 +36,10 @@ main() {
       checkFormatoNovedad $next_file $contador "$novedad" || continue
 
       # Validar campos
-      checkCamposNovedad $next_file $contador "$novedad" || continue
+      checkCamposNovedad $next_file $contador "$novedad"
 
       # Calcular campos
-      generarBeneficio "$novedad" >> novedades.$$
+      generarBeneficio $next_file "$novedad" >> novedades.$$
     done < "$next_file"
 
     # Enviar a recibidos
@@ -58,17 +57,22 @@ checkFormatoNovedad() {
   cantidad_de_campos=$(( `echo "$3" | grep -o ',' | wc -l` + 1 ))
 
   if [[ $cantidad_de_campos -eq 11 ]]; then
-    splittable=`echo "$3" | sed "s/\([^,]*\)/'\1'/g" | tr ',' ' '`
+
+    # Cambiar las comas por espacios y los espacios por comas
+    # splittable=`echo "$3" | tr ' ,' ', '`
 
     # Convertir la novedad en un array
     ARRAY_NOVEDAD=()
-    for campo in $splittable; do
-      # XXX: Check if can do this assignment without seding the quotes
-      ARRAY_NOVEDAD[${#ARRAY_NOVEDAD[*]} + 1]=`echo "$campo" | sed "s/'//g"`
+    for i in {1..11}; do
+      ARRAY_NOVEDAD[$i-1]=`echo $3 | cut -f $i -d ,`
     done
+    # for campo in $splittable; do
+    #   # Cargar el campo en el arreglo corrigiendo las comas por los espacios originales
+    #   ARRAY_NOVEDAD[${#ARRAY_NOVEDAD[@]}]="`echo $campo | tr , ' '`"
+    # done
 
     # Validar campos obligatorios
-    for i in {1..9}; do
+    for i in {0..8}; do
       if [[ -z ${ARRAY_NOVEDAD[$i]} ]]; then
         rechazarRegistro "$@" "Campo ${CAMPOS_NOVEDAD[$i]} no informado"
         error='true'
@@ -89,41 +93,94 @@ checkFormatoNovedad() {
 
 checkCamposNovedad() {
 
-  $datos_beneficio=`buscar_beneficio ${ARRAY_NOVEDAD[8]}`
-  if [[ ! -z "$datos_agencia" ]]; then
-    rechazarRegistro "$@" "No existe beneficio ${ARRAY_NOVEDAD[8]}"
-    return 0
+  unset ESTADO_RECHAZO MOTIVOS_RECHAZO
+
+  # Validar campos obligatorios
+  for i in {0..8}; do
+    if [[ -z ${ARRAY_NOVEDAD[$i]} ]]; then
+      rechazarBeneficio "Campo ${CAMPOS_NOVEDAD[$i]} no informado"
+    fi
+  done
+
+  datos_beneficio=`buscar_beneficio ${ARRAY_NOVEDAD[8]}`
+  if [[ -z "$datos_beneficio" ]]; then
+    rechazarBeneficio "No existe beneficio ${ARRAY_NOVEDAD[8]}"
   fi
 
-  if [[ ! `verificarFecha ${ARRAY_NOVEDAD[10]}` ]]; then
-    rechazarRegistro "$@" "FPA invalida ${ARRAY_NOVEDAD[10]}"
-    return 0
+  if [[ -n "${ARRAY_NOVEDAD[9]}" ]]; then
+    if [[ ! `verificarFecha ${ARRAY_NOVEDAD[9]}` ]]; then
+      rechazarBeneficio "FPA invalida ${ARRAY_NOVEDAD[9]}"
+    fi
   fi
 
-  FPB=$(fechaBajaBeneficio "$datos_beneficio")
-  if [[ `fechaEnSegundos "${ARRAY_NOVEDAD[10]}"` -gt \
-        `fechaEnSegundos "$FPB"`
-    ]]; then
-    rechazarRegistro "$@" "FPA (${ARRAY_NOVEDAD[10]}) mayor que FPB ($FPB)"
-    return 0
+  FPB=$(fecha_baja_beneficio "$datos_beneficio")
+  if [[ "${ARRAY_NOVEDAD[9]}" > "$FPB" ]]; then
+    rechazarBeneficio "FPA (${ARRAY_NOVEDAD[9]}) mayor que FPB ($FPB)"
   fi
 
-  DMB=$(duracionMaximaBeneficio "$datos_beneficio")
-  if [[ ${ARRAY_NOVEDAD[11]-0} -gt $DMB ]]; then
-    rechazarRegistro "$@" "DP (${ARRAY_NOVEDAD[11]}) mayor que DMB ($DMB)"
-    return 0
+  DMB=$(duracion_maxima_beneficio "$datos_beneficio")
+  if [[ ${ARRAY_NOVEDAD[10]:-0} -gt $DMB ]]; then
+    rechazarBeneficio "DP (${ARRAY_NOVEDAD[10]}) mayor que DMB ($DMB)"
   fi
 
-  if [[ ${ARRAY_NOVEDAD[11]-1} -le 0 ]]; then
-    rechazarRegistro "$@" "DP (${ARRAY_NOVEDAD[11]}) menor o igual que 0"
-    return 0
+  if [[ ${ARRAY_NOVEDAD[10]:-1} -le 0 ]]; then
+    rechazarBeneficio "DP (${ARRAY_NOVEDAD[10]}) menor o igual que 0"
   fi
 
-  true
 }
 
 generarBeneficio() {
-  echo "hola"
+
+  FC=`date +'%Y-%m-%d'`
+
+  FPA=${ARRAY_NOVEDAD[9]}
+
+  FIB=`fecha_inicio_beneficio "$datos_beneficio"`
+
+  FEA=`mayor $FC $FPA $FIB`
+
+  if [[ -n "$ESTADO_RECHAZO" ]]; then
+    ESTADO=rechazado
+  elif [[ "$FEA" > "$FC" ]]; then
+    ESTADO=pendiente
+  else
+    ESTADO=aprobado
+  fi
+
+  DMB=`duracion_maxima_beneficio "$datos_beneficio"`
+  DURACION=${ARRAY_NOVEDAD[10]:-$DMB}
+
+  FF=`menor $(sumarMeses $FEA $DURACION) $(fecha_baja_beneficio "$datos_beneficio")`
+
+  # Agencia, del nombre del archivo de postulantes
+  # Secuencia, del nombre del archivo de postulantes
+  echo -n `echo $1 | sed 's/\.\([^.]*\)$/,\1/'`,
+  # Los 10 primeros campos del archivo de postulantes (sin modificar)
+  # Cuil, del archivo de postulantes
+  # Tipo doc, del archivo de postulantes
+  # Nro doc, del archivo de postulantes
+  # Apellido, del archivo de postulantes
+  # Nombre, del archivo de postulantes
+  # Domicilio, del archivo de postulantes
+  # Localidad, del archivo de postulantes
+  # Provincia, del archivo de postulantes
+  # Código de beneficio, del archivo de postulantes
+  # Fecha Pedida de Alta , del archivo de postulantes
+  echo -n "`echo $2 | cut -f 1-10 -d ,`",
+  # Fecha Efectiva de Alta, calculada
+  echo -n $FEA,
+  # Estado, calculado, valores posibles: rechazado o aceptado o pendiente
+  echo -n $ESTADO,
+  # Duración, del archivo de postulantes o calculada
+  echo -n $DURACION,
+  # Fecha Finalización, calculada
+  echo -n $FF,
+  # Motivo, N caracteres, explica el motivo del estado asignado
+  echo -n $MOTIVOS_RECHAZO,
+  # Usuario, calculado, nombre del usuario
+  echo -n `whoami`,
+  # Fecha Corriente, calculada, fecha actual
+  echo $FC
 }
 
 rechazarRegistro() {
@@ -135,6 +192,11 @@ rechazarRegistro() {
   echo "param 2 $2"
   echo "param 3 $3"
   echo "param 4 $4"
+}
+
+rechazarBeneficio() {
+  MOTIVOS_RECHAZO=$MOTIVOS_RECHAZO${MOTIVOS_RECHAZO:+"|"}$1
+  ESTADO_RECHAZO=1
 }
 
 main
