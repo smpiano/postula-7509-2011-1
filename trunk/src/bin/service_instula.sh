5915e40a33393a431f9afc94641d2c468c975508
smpiano@gmail.com <smpiano@gmail.com@aeaa8df5-d76f-69f6-2d0c-b8b2e875e448>
2011-05-04 03:41:26 +0000

    Agrego un servicio que ayuda a pedir y setear los datos sobre el archivo de configuracion de instula.conf. Lo mas probable es que sea usado por el instalador, postini y plist.pl

---
+++ trunk/src/bin/service_instula.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# SERVICIO DESTINADO A MANEJAR EL ARCHIVO DE CONFIGURACION DE INSTULA
+#
+# 1- Un solo parametro indica un GET sobre la variable de entorno
+# 2- Dos parametros indica un SET sobre la variable o la creacion,
+#    en caso de que no exista
+
+# Valido que se pasen 1 o 2 parametros
+if [ $# != 1 -a $# != 2 ]
+then
+	exit 1
+fi
+
+NOMBRE=$1
+VALOR=$2
+
+# Indico la posicion del archivo de configuracion
+INSTULA_CONF="$PWD/instula.conf"
+if [ ! -f "$INSTULA_CONF" ]
+then
+	>>$INSTULA_CONF
+fi
+
+# Evaluo que exista el nombre de esa variable
+EXISTE_VARIABLE=`cat $INSTULA_CONF | grep "$NOMBRE"`
+
+# Si el valor contiene algo significa que quieren hacer un SET
+if [ -z  "$VALOR" ]
+then
+	local path=`echo $EXISTE_VARIABLE | cut -f2 -d\=`
+	echo $path
+else
+	if [ -z "$EXISTE_VARIABLE" ]
+	then
+		# Si no existe la variable la escribo en el archivo
+		echo "$NOMBRE=$VALOR">>$INSTULA_CONF
+	else
+		# Modifico el valor de la variable
+		sed -i "s/$NOMBRE.*/$NOMBRE=$VALOR/" $INSTULA_CONF
+	fi
+fi
