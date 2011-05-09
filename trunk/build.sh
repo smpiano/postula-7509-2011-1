7b13f47b6387528a66c32f8eeb956b8c6d567f71
marceloruiz.fiuba@gmail.com <marceloruiz.fiuba@gmail.com@aeaa8df5-d76f-69f6-2d0c-b8b2e875e448>
2011-05-09 23:34:36 +0000

    subo el script para crear el tar

---
+++ trunk/build.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+NOMBRE_TAR='build.tar'
+
+# borra el tar si ya existe
+if [ -e "$NOMBRE_TAR" ]
+then
+  rm "$NOMBRE_TAR"
+fi
+
+# borra los directorio si ya existen
+if [ -e conf ]
+then
+  rm -r conf
+fi
+
+if [ -e data ]
+then
+  rm -r data
+fi
+
+if [ -e inst ]
+then
+  rm -r inst
+fi
+
+# crea los directorios
+mkdir conf
+mkdir data
+mkdir inst
+
+# copia los archivos
+cp -ur src/data/* data
+
+cp -ur src/instalador/* inst
+cp -ur src/bin/* inst
+
+# borra los ocultos del svn
+cd data
+rm -rf `find . -type d -name .svn`
+cd ..
+
+cd inst
+rm -rf `find . -type d -name .svn`
+cd ..
+
+# crea el tar
+tar czf $NOMBRE_TAR conf data inst
+
+# borra los directorios
+rm -r conf
+rm -r data
+rm -r inst
+
