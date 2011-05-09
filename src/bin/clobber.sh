099238d848b700f3c868680e69b7d531c4088c86
snipperme@gmail.com <snipperme@gmail.com@aeaa8df5-d76f-69f6-2d0c-b8b2e875e448>
2011-05-09 02:58:10 +0000

    Added clobber to clean and load_test.sh to copy data to arribos

--- src/bin/clobber.sh
+++ src/bin/clobber.sh
@@ -1,6 +1,7 @@
-root_dir=${1:-$PWD/grupo02}
-rm $root_dir/benef.*
-rm $root_dir/benerro.*
+[[ $# -eq 0 ]] && echo "Debe informar el directorio root a limpiar" && exit 1
+
+root_dir=${1%/}
+rm $root_dir/nuevos/benef.*
+rm $root_dir/nuevos/benerro.*
 rm $root_dir/logs/*
-mv $root_dir/procesados/* $root_dir/arribos
-mv $root_dir/rechazados/* $root_dir/arribos
+mv $root_dir/procesados/* $root_dir/rechazados/* $root_dir/recibidos/* $root_dir/arribos 2> /dev/null
