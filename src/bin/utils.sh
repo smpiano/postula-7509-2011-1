46ec74a9310f1e1f9a770c7d30f542de2f285d7b
snipperme@gmail.com <snipperme@gmail.com@aeaa8df5-d76f-69f6-2d0c-b8b2e875e448>
2011-05-10 18:39:59 +0000

    Fixed some utils function

--- src/bin/utils.sh
+++ src/bin/utils.sh
@@ -19,18 +19,23 @@ error_severo() {
   log "$1" "ES" "${@:2}"
 }
 
-checkCurrentScriptAlreadyRunning() {
+checkScriptAlreadyRunning() {
+  
   # Listar todos los comandos | filtrar los que tienen el nombre de archivo y no son 'grep' | contar la cantidad de comandos
-  count=`ps axo 'pid=,command=' | grep -v grep | grep -c -e "$0"`
+  count=`ps axo 'pid=,command=' | grep -v grep | grep -c -e "$1"`
   # Descontar el fork creado por el backtick
-  count=$(( $count - 1 ))
 
-  if [[ ! $count -eq 1 ]]
+  if [[ ! $count -eq ${2:-0} ]]
   then
-    error "process is already running $0 `ps eo command='' | grep -v grep`"
+    error "process $1 is running $count times"
     # Salir con false para poder manejar cancelación del script
     false
   fi
+}
+checkCurrentScriptAlreadyRunning() {
+  # La verificacion de si el script actual esta corriendo usa un fork, por ende
+  # si estoy corriendo solo tengo que ver exactamente 2 procesos
+  checkScriptAlreadyRunning "`basename $0`" 2
 }
 
 checkEnvironmentLoaded() {
@@ -72,7 +77,7 @@ verificarFecha() {
   if [ "$TERM_PROGRAM" = "Apple_Terminal" ]; then
     date -j -f "%Y-%m-%d" "$1" 2> /dev/null
   else
-    date +"%Y-%m-%d" --date "$1"
+    date +"%Y-%m-%d" --date "$1" 2> /dev/null
   fi
 }
 
@@ -81,7 +86,7 @@ sumarMeses() {
     fecha_en_segundos=`date -j -f "%Y-%m-%d" "$1" +"%s" 2> /dev/null || date +"%s"`
     date -j -r $(expr $fecha_en_segundos + ${2:-0} \* 2592000 2> /dev/null || echo "0") +"%Y-%m-%d" 2> /dev/null || $1
   else
-    date --date "$1 +$2 month"
+    date --date "$1 +$2 month" 2> /dev/null
   fi
 }
 
