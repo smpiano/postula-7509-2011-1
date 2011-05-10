2f10bad933172b1d19efb7a5fc627944a7f06c9a
smpiano@gmail.com <smpiano@gmail.com@aeaa8df5-d76f-69f6-2d0c-b8b2e875e448>
2011-05-10 04:27:57 +0000

    Agrego un borrador de lo que hace el instalador. Agregue la carga del directorio procesados que no lo estaba creando instula, y modifico validacion de Sizes.

---
+++ trunk/src/instalador/inst/cleaner.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Este script pretende limpiar las estructura de directorios que creo el comando de instalacion de instula
+# Correrlo desde donde se encuentra service_instula_conf
+
+echoVars () {
+  echo "$1=$2"
+}
+
+CURRDIR="`./service_instula_conf.sh  CURRDIR`" # CURRDIR no se borra.
+ARRIDIR="`./service_instula_conf.sh  ARRIDIR`"
+BINDIR="`./service_instula_conf.sh  BINDIR`"
+CONFDIR="`./service_instula_conf.sh  CONFDIR`"
+LOGDIR="`./service_instula_conf.sh  LOGDIR`"
+DATADIR="`./service_instula_conf.sh  DATADIR`"
+NUEVOS="`./service_instula_conf.sh  NUEVOS`"
+RECIBIDOS="`./service_instula_conf.sh  RECIBIDOS`"
+PROCESADOS="`./service_instula_conf.sh  PROCESADOS`"
+RECHAZADOS="`./service_instula_conf.sh  RECHAZADOS`"
+
+
+if [ -d "$CURRDIR" -a -d "$ARRIDIR" -a -d "$BINDIR" -a -d "$CONFDIR" -a -d "$LOGDIR" -a -d "$DATADIR" -a -d "$NUEVOS" -a -d "$RECIBIDOS" -a -d "$PROCESADOS" -a -d "$RECHAZADOS" ]
+then
+  echo "Borrando estructuras.."
+  rm -rfv "$ARRIDIR"
+  rm -rfv "$LOGDIR"
+  rm -rfv "$DATADIR"
+  rm -rfv "$CURRDIR/inst"
+  rm -rfv "$NUEVOS"
+  rm -rfv "$RECIBIDOS"
+  rm -rfv "$PROCESADOS"
+  rm -rfv "$RECHAZADOS"
+  rm -rfv "$CONFDIR"
+  rm -v "$BINDIR/*.sh"
+else
+  echo "No puede borrar alguno de los directorios no esta bien configurado en instula.conf"
+  echo "ARRIDIR=$ARRIDIR"
+  echo "LOGDIR=$LOGDIR"
+  echo "DATADIR=$DATADIR"
+  echo "NUEVOS=$NUEVOS"
+  echo "RECIBIDOS=$RECIBIDOS"
+  echo "PROCESADOS=$PROCESADOS"
+  echo "RECHAZADOS=$RECHAZADOS"
+  echo "CONFDIR=$CONFDIR"
+  echo "BINDIR=$BINDIR"
+fi
--- trunk/src/instalador/inst/creacionDirectorioGrupoInstula.sh
+++ trunk/src/instalador/inst/creacionDirectorioGrupoInstula.sh
@@ -2,20 +2,14 @@
 #En el caso de que lo sea, no hace nada
 #En el caso de que no lo sea, crea dentro del directorio actual el nuevo directorio
 
-NUEVO_DIRECTORIO=$1 
+NUEVO_SUBDIRECTORIO=$1 
 RUTA_A_CHECKEAR=$2
-EVALUAR=`echo $RUTA_A_CHECKEAR | grep ".*/$NUEVO_DIRECTORIO\$"`
-if [ -z "$EVALUAR" ]
+CURRENT="$RUTA_A_CHECKEAR/$NUEVO_SUBDIRECTORIO"
+if [ ! -d "$CURRENT" ]
 then
-	CURRENT="$RUTA_A_CHECKEAR/$NUEVO_DIRECTORIO"
-	echo "El directorio \"$RUTA_A_CHECKEAR\" no contiene como ultmo directorio \"$NUEVO_DIRECTORIO\""
-	if [ ! -d $CURRENT ]
-	then
-		mkdir -p $CURRENT;
-		echo "Se creo el directorio, \"$CURRENT\".."
-	else
-		echo "El directorio \"$CURRENT\" ya existe."
-	fi
+	echo "El directorio \"$RUTA_A_CHECKEAR\" no contiene como ultmo directorio a \"$NUEVO_SUBDIRECTORIO\""
+	mkdir -p $CURRENT;
+	echo "Se creo el directorio, \"$CURRENT\".."
 else
-	echo "El directorio \"$RUTA_A_CHECKEAR\" es el buscado"
+	echo "El directorio \"$CURRENT\" ya existe."
 fi
